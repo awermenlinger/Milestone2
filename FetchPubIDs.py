66c9d6c24999396c27843bc0a5078c0ff116494e
Nywroc <a.wermenlinger@gmail.com>
2021-07-25 17:32:45 +0200

    Added the code to get the abstract, works with ~25 articles. Also fixed the output of the FetchPubIDs

--- FetchPubIDs.py
+++ FetchPubIDs.py
@@ -1,7 +1,8 @@
 from Bio import Entrez
 import pandas as pd
+import numpy as np
 
-# Goal of this python script is to fetch the pubids from pubmed and save them to a csv file
+# Goal of this python script is to fetch the pubids from pubmed and save them to a txt file
 
 my_email = 'awerm@umich.edu'
 API = "ee71d15e8a446320e3aaacd01c88966efe08"
@@ -21,11 +22,12 @@ def search(query):
     results = Entrez.read(handle)
     return results
 
-
 #query = "(\"biotechnology\"[MeSH Terms] OR \"biotechnology\"[All Fields] OR \"biotechnologies\"[All Fields]) AND ((fha[Filter]) AND (clinicaltrial[Filter] OR randomizedcontrolledtrial[Filter]))"
 query = "biotechnology AND has abstract[FILT] AND (clinical trial[FILT] OR randomized controlled trial[FILT])"
 
 results = search(query)
 id_list = results['IdList']
-df_temp = pd.DataFrame(id_list)
-df_temp.to_csv("Milestone2\pubmed_ids_from_search.csv")
+
+with open('pubmed_ids_from_search.txt', 'w') as f:
+    for item in id_list:
+        f.write("%s\n" % item)
