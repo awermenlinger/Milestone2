6c705e3f4a74cddd461518409832a4eb4012fce5
mcmanus-git <melancholic_maclean@yahoo.com>
2021-07-29 22:39:38 -0400

    Test read in csv from parent folder

--- mike_experiments/test.py
+++ mike_experiments/test.py
@@ -1,1 +1,9 @@
-print('hello')
+# with open("../pubmed_ids_extracted_abstracts.csv") as file:
+#     lines = file.readlines()
+#
+# print(lines)
+
+import pandas as pd
+
+df = pd.read_csv("../pubmed_ids_extracted_abstracts.csv")
+print(df)
