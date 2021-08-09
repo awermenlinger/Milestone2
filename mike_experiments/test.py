efc29aa0930c47bcfa1f11342e5bc84f793e20a1
mcmanus-git <melancholic_maclean@yahoo.com>
2021-08-08 23:15:55 -0400

    added successful test to pull MeSH terms and Keywords from articles to test.py

--- mike_experiments/test.py
+++ mike_experiments/test.py
@@ -3,7 +3,25 @@
 #
 # print(lines)
 
-import pandas as pd
+# import pandas as pd
+#
+# df = pd.read_csv("../pubmed_ids_extracted_abstracts.csv")
+# print(df)
 
-df = pd.read_csv("../pubmed_ids_extracted_abstracts.csv")
-print(df)
+from Bio import Entrez, Medline
+from mike_experiments.config import *
+
+Entrez.email = mike_email
+Entrez.api_key = mike_api_key
+
+
+
+
+pbmdid = 34281299
+
+handle = Entrez.efetch(db='pubmed', rettype='medline', retmode='text', id=pbmdid)
+article = Medline.parse(handle)
+article = list(article)
+key_words = article[0].get('OT')
+mesh_terms = article[0].get('MH')
+print(article[0].get('MH'))
