87f51e9d0e7eaf62316a4d0d93761788280a36da
Antoine W <a.wermenlinger@gmail.com>
2021-09-06 18:09:57 +0200

    Fixed LDA

--- LDA_Topics_and_Vis.py
+++ LDA_Topics_and_Vis.py
@@ -6,6 +6,7 @@ import pyLDAvis.gensim_models
 import pickle
 import os
 
+
 #Some code inspired from https://towardsdatascience.com/end-to-end-topic-modeling-in-python-latent-dirichlet-allocation-lda-35ce4ed6b3e0
 
 
@@ -17,22 +18,37 @@ def preprocess(text):
          result.append(token)
    return result
 
-num_topics=10
-original_df = pd.read_csv("data/pubmed_articles.csv")
-original_df.abstract = original_df.abstract.astype('str')
-input_data = original_df.copy()
+num_topics=20
 
+#load the files
+df1 = pd.read_csv("data/pubmed_articles_cancer_01.csv",skip_blank_lines=True)
+df1.dropna(inplace=True, axis = 0, how = 'all')
+df2 = pd.read_csv("data/pubmed_articles_cancer_02.csv")
+df2.dropna(inplace=True, axis = 0, how = 'all')
+df3 = pd.read_csv("data/pubmed_articles_cancer_03.csv")
+df3.dropna(inplace=True, axis = 0, how = 'all')
+df4 = pd.read_csv("data/pubmed_articles_cancer_04.csv")
+df4.dropna(inplace=True, axis = 0, how = 'all')
+input_data = pd.DataFrame().append([df1,df2,df3, df4])
+
+#ensure clean abstracts
+input_data.abstract = input_data.abstract.astype('str')
+
+#preprocess abstracts
 doc_processed = input_data['abstract'].map(preprocess)
 
+#build the dictionary
 dictionary = corpora.Dictionary(doc_processed)
+
 #to prepapre a document term matrix
 doc_term_matrix = [dictionary.doc2bow(doc) for doc in doc_processed]
 
 #Lda model
 Lda = gensim.models.ldamodel.LdaModel
+
 #Lda model to get the num_topics, number of topic required, 
 #passses is the number training do you want to perform
-ldamodel = Lda(doc_term_matrix, num_topics=10, id2word = dictionary, passes=2)
+ldamodel = Lda(doc_term_matrix, num_topics=num_topics, id2word = dictionary, passes=2)
 
 # Visualize the topics
 #pyLDAvis.enable_notebook() only works in ipython - will save to html
