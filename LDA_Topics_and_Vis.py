a4a91a9c9aaba66800e17fb0928213f367443367
Nywroc <a.wermenlinger@gmail.com>
2021-09-08 23:58:22 +0200

    Data prep for LDA over time vis

--- LDA_Topics_and_Vis.py
+++ LDA_Topics_and_Vis.py
@@ -5,6 +5,11 @@ import pyLDAvis
 import pyLDAvis.gensim_models
 import pickle
 import os
+import logging
+
+#http://www.cse.chalmers.se/~richajo/dit862/L13/LDA%20with%20gensim%20(small%20example).html
+# for gensim to output some progress information while it's training
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)  
 
 
 #Some code inspired from https://towardsdatascience.com/end-to-end-topic-modeling-in-python-latent-dirichlet-allocation-lda-35ce4ed6b3e0
@@ -23,13 +28,13 @@ def preprocess(text):
 
 
 #load the files
-df1 = pd.read_csv("data/pubmed_articles_cancer_01.csv",skip_blank_lines=True)
+df1 = pd.read_csv("data/pubmed_articles_cancer_01_smaller.csv",skip_blank_lines=True)
 df1.dropna(inplace=True, axis = 0, how = 'all')
-df2 = pd.read_csv("data/pubmed_articles_cancer_02.csv")
+df2 = pd.read_csv("data/pubmed_articles_cancer_02_smaller.csv")
 df2.dropna(inplace=True, axis = 0, how = 'all')
-df3 = pd.read_csv("data/pubmed_articles_cancer_03.csv")
+df3 = pd.read_csv("data/pubmed_articles_cancer_03_smaller.csv")
 df3.dropna(inplace=True, axis = 0, how = 'all')
-df4 = pd.read_csv("data/pubmed_articles_cancer_04.csv")
+df4 = pd.read_csv("data/pubmed_articles_cancer_04_smaller.csv")
 df4.dropna(inplace=True, axis = 0, how = 'all')
 input_data = pd.DataFrame().append([df1,df2,df3, df4])
 
@@ -41,28 +46,34 @@ doc_processed = input_data['abstract'].map(preprocess)
 
 #build the dictionary
 dictionary = corpora.Dictionary(doc_processed)
+#save the dictionary
+filename = 'models/trained_lda_dictionary.sav'
+pickle.dump(dictionary, open(filename, 'wb'))      
 
 #to prepapre a document term matrix
-doc_term_matrix = [dictionary.doc2bow(doc) for doc in doc_processed]
+corpus = [dictionary.doc2bow(doc) for doc in doc_processed]
+#save the corpus
+filename = 'models/trained_lda_corpus.sav'
+pickle.dump(corpus, open(filename, 'wb'))      
 
 #Lda model
 LDA = gensim.models.ldamodel.LdaModel
 
 #Lda model with settings
-ldamodel = LDA(doc_term_matrix, num_topics=num_topics, id2word = dictionary, passes=passes, random_state=RANDOM_SEED)
+ldamodel = LDA(corpus, num_topics=num_topics, id2word = dictionary, passes=passes, random_state=RANDOM_SEED)
 
 #Save the LDA Model
 filename = 'models/trained_lda.sav'
-pickle.dump(ldamodel, open(filename, 'wb'))
+pickle.dump(ldamodel, open(filename, 'wb'))                                        
 
-
-# Visualize the topics
+#Visualize the topics
 LDAvis_data_filepath = os.path.join('./results/ldavis_prepared_'+str(num_topics))
 
 LDAvis_prepared = pyLDAvis.gensim_models.prepare(ldamodel, doc_term_matrix, dictionary)
 with open(LDAvis_data_filepath, 'wb') as f:
-    pickle.dump(LDAvis_prepared, f)
-# load the pre-prepared pyLDAvis data from disk
+   pickle.dump(LDAvis_prepared, f)
+
+#Load the pre-prepared pyLDAvis data from disk
 with open(LDAvis_data_filepath, 'rb') as f:
-    LDAvis_prepared = pickle.load(f)
+   LDAvis_prepared = pickle.load(f)
 pyLDAvis.save_html(LDAvis_prepared, './results/ldavis_prepared_'+ str(num_topics) +'.html') 
