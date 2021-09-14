0377156b84defe308b5f4852fafde05797604448
Nywroc <a.wermenlinger@gmail.com>
2021-09-14 15:33:19 +0200

    top 2 vec

--- top2vec_model_and_vis.py
+++ top2vec_model_and_vis.py
@@ -20,9 +20,13 @@ top2vec_file = "models/Top2Vec_model.sav"
 
 model = Top2Vec(documents=doc_processed, embedding_model = "universal-sentence-encoder", speed="learn", workers=8)
 #Save the Top2Vec Model
-model.save("top2vec_file")
+model.save(top2vec_file)
 
 topic_words, word_scores, topic_nums = model.get_topics(19)
 
 for topic in topic_nums:
     model.generate_topic_wordcloud(topic)
+    #save the matplotlib to png --worked with Colab for report
+
+
+#
